ea05816903c801764a88faa52b4a9c81dca8c657
asprin107 <asprin107@gmail.com>
2022-07-08 02:50:29 +0900

    edit lambda module

--- iac/quick/lambda/data.tf
+++ iac/quick/lambda/data.tf
@@ -1,19 +1,7 @@
 data "aws_iam_policy_document" "lambda_svc_policy" {
   statement {
-
-  }
-}
-
-data "aws_subnet" "lambda_subnet" {
-  filter {
-    name   = ""
-    values = []
-  }
-}
-
-data "aws_security_groups" "lambda_security_group" {
-  filter {
-    name   = ""
-    values = []
+    effect = "Allow"
+    actions = ["s3:*"]
+    resources = ["*"]
   }
 }
