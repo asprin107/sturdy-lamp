9db8977bdfc896c3d5d61f6b6c3ce5f0df2cad24
asprin107 <asprin107@gmail.com>
2023-02-03 19:43:20 +0900

    Add jmeter worker user data.

--- iac/jmeter/worker-linux/user_data.sh
+++ iac/jmeter/worker-linux/user_data.sh
@@ -1,2 +1,34 @@
 #!/bin/sh
 
+yum update -y
+#yum install -y wget
+
+echo "Create jmeter home directory"
+mkdir -p /JmeterHome
+
+echo "Download jmeter"
+wget https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-5.5.zip -P /JmeterHome
+
+echo "Unzip jmeter"
+unzip /JmeterHome/apache-jmeter-5.5.zip -d /JmeterHome
+
+echo "Download jmeter plugin 'plugin-manager'"
+wget https://jmeter-plugins.org/get -P /JmeterHome/apache-jmeter-5.5/lib/ext
+
+echo "Download jmeter plugin 'jpgc-graphs-basic'"
+wget https://jmeter-plugins.org/files/packages/jpgc-graphs-basic-2.0.zip -P /JmeterHome
+unzip /JmeterHome/jpgc-graphs-basic-2.0.zip -d /JmeterHome
+cp -r /JmeterHome/lib/* /JmeterHome/apache-jmeter-5.5/lib/
+
+echo "Download jdk8"
+wget https://cdn.azul.com/zulu/bin/zulu8.68.0.21-ca-jdk8.0.362-linux_x64.tar.gz -P /JmeterHome
+
+echo "Unzip jdk8"
+tar -zxf /JmeterHome/zulu8.68.0.21-ca-jdk8.0.362-linux_x64.tar.gz -C /JmeterHome
+
+echo "Add jdk8 in Environment 'Path'"
+echo '
+# Add Java home environment
+export JAVA_HOME=\/JmeterHome/zulu8.68.0.21-ca-jdk8.0.362-linux_x64"
+export PATH=$JAVA_HOME/bin:$PATH
+' >> /etc/profile
